306c2af2088f2c14e871dc5bc36e9325053c0c54
Nemu64 <35722540+Nemu64@users.noreply.github.com>
2020-08-21 08:02:31 -0400

    s

--- iamgod/god.js
+++ iamgod/god.js
@@ -10,9 +10,6 @@ setTimeout(function() {
                 god.innerHTML = "Informing your computer that I am God...";
                 setTimeout(function() {
                     god.innerHTML = "Informing your computer that I am God... OK";
-                    setTimeout(function() {
-                        god.innerHTML = "What did you expect?";
-                    }, delay);
                 }, delay);
             }, delay);
         }, delay);
