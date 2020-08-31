70f8eda9ab9fbbf1e10e8fa6a4d68131cf60d946
Nemu64 <35722540+Nemu64@users.noreply.github.com>
2020-08-30 23:47:34 -0400

    Update clockmessage.js

--- swf/js/clockmessage.js
+++ swf/js/clockmessage.js
@@ -2,7 +2,7 @@ var date=new Date();
 var month=date.getMonth();
 var day=date.getDate();
 function datedependent() {
-setTimeout(datedependent2, 2000);
+setTimeout(datedependent2, 0);
 }
 function datedependent2(){
     var div=document.getElementById('splash');
