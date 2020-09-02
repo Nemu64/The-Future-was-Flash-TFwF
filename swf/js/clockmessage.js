be2dac8b41c51dda299bac555b3fec82b572f4cf
Nemu64 <35722540+Nemu64@users.noreply.github.com>
2020-09-02 11:38:38 -0400

    Update clockmessage.js

--- swf/js/clockmessage.js
+++ swf/js/clockmessage.js
@@ -2,11 +2,11 @@ var date=new Date();
 var month=date.getMonth();
 var day=date.getDate();
 function datedependent() {
-    setTimeout(datedependent2, 100);
+    setTimeout(datedependent2, 50);
 }
 function datedependent2() {
     var div=document.getElementById('splash');
-    if (month==11) {
+    if (month==11) { /*December*/
         if (day==18) {
             div.innerHTML='Happy Birthday Nemu!';
         }
@@ -17,7 +17,7 @@ function datedependent2() {
             div.innerHTML='Happy New Year’s Eve!';
         }
     }
-    else if (month==06) {
+    else if (month==06) { /*July*/
         if (day==25) {
             div.innerHTML='Happy Birthday Nick!';
         }
@@ -25,7 +25,7 @@ function datedependent2() {
             div.innerHTML='Happy Birthday Mom!';
         }
     }
-    else if (month==05) {
+    else if (month==05) { /*June*/
         if (day==23) {
             div.innerHTML='Happy Birthday Sonic!';
         }
@@ -33,24 +33,34 @@ function datedependent2() {
             div.innerHTML='Happy Birthday Sega!';
         }
     }
-    else if (month==0) {
+    else if (month==0) { /*January*/
         if (day==1) {
             div.innerHTML='Happy Birthday Flash Player!';
         }
     }
-    else if (month==4) {
+    else if (month==4) { /*May*/
         if (day==7) {
             div.innerHTML='Happy Birthday Sony!';
         }
     }
-    else if (month==10) {
+    else if (month==10) { /*November*/
         if (day==20) {
             div.innerHTML='Happy Birthday Windows!';
         }
     }
-    else if (month==7) {
+    else if (month==7) { /*August*/
         if (day==30) {
             div.innerHTML='Happy Birthday Cassettes!';
+        }
+    }
+    else if (month==8) { /*September*/
+        if (day==11) {
+            div.innerHTML='Never Forget!';
+        }
+    }
+    else if (month==9) { /*October*/
+        if (day==25) {
+            div.innerHTML='Happy Birthday Word!';
         }
     }
 }
