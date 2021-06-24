21a10b5aeb172728c874e4c9738e1d381ceb017a
Nemu64 <nemu@ipxforever.com>
2021-06-23 20:06:13 -0400

    allow overriding ruffle

--- swf/ruffle/rufflesetup.js
+++ swf/ruffle/rufflesetup.js
@@ -1,6 +1,10 @@
+const useflashplayer = new URLSearchParams(window.location.search);
+var useflash = bdinfo.get('forceflash');
+if (forceflash != "true" || forceflash != "1"){
 window.RufflePlayer = window.RufflePlayer || {};
 window.RufflePlayer.config = {
     "polyfills": true,
     "autoplay": "on",
     "unmuteOverlay": "hidden",
+}
 }
