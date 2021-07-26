803cf2a2bec9289d250d4b5d36ecdd207426cd8f
Nemu64 <nemu@ipxforever.com>
2021-07-26 14:44:51 -0400

    remove unnecessary files

--- rps/rps.js
+++
@@ -1,7 +0,0 @@
-var status = document.getElementById('status');
-var comscore = document.getElementById('comscore');
-var playerscore = document.getElementById('playerscore');
-var dev = document.getElementById('comscore');
-function rock() {
-dev.innerHTML='Hello world!';
-}
